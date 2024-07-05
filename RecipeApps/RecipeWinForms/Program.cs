8e409eaae99409390a62f10482256c0ee5d5373e
ruchybrach <brach4a@gmail.com>
2024-07-05 19:42:41 -0400

    Complete

--- RecipeApps/RecipeWinForms/Program.cs
+++ RecipeApps/RecipeWinForms/Program.cs
@@ -13,7 +13,7 @@ namespace RecipeWinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            SQLUtility.ConnectionString = "Server=tcp:r-b.database.windows.net,1433;Initial Catalog=HeartyHearthDB;Persist Security Info=False;User ID=rbadmin;Password=rbad@#8580;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SQLUtility.ConnectionString = "";
             Application.Run(new frmSearch());
         }
     }
