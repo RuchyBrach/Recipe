eb6f24067e5659f5c5a9c851c81f50cd1908e0b0
ruchybrach <brach4a@gmail.com>
2024-09-19 07:39:52 -0400

    edited

---
+++ RecipeApps/RecipeTest/RecipeTest.cs
@@ -0,0 +1,16 @@
+namespace RecipeTest
+{
+    public class RecipeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
